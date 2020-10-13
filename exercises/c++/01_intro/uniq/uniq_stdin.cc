f4902691c330c712ad054d71b5d368669a0161d1
Matteo Poggi <matteo.poggi.fi@gmail.com>
2020-10-13 19:39:39 +0200

    Add: uniq.

---
+++ exercises/c++/01_intro/uniq/uniq_stdin.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+void print_count(const int c, const std::string& s)
+{
+    std::cout << "\t" << c << ": " << s << std::endl;
+}
+
+int main()
+{
+
+    std::string curr, prev;
+    uint64_t i{1};
+
+    if(!std::getline(std::cin, curr))
+        return 0;
+    prev = curr;
+    while(std::getline(std::cin, curr)) {
+        if(curr == prev)
+            ++i;
+        else {
+            print_count(i, prev);
+            i = 1;
+        }
+        prev = curr;
+    }
+    print_count(i, prev);
+
+    return 0;
+}
