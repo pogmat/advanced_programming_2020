e174388563f8f87117ac88fbe7b11526ca80193c
Matteo Poggi <matteo.poggi.fi@gmail.com>
2020-10-15 14:19:27 +0200

    Remove istringsream solution. Add isatty option

--- exercises/c++/01_intro/getters/getters.cc
+++ exercises/c++/01_intro/getters/getters.cc
@@ -1,40 +1,10 @@
 #include <iostream>
 #include <string>
-#include <sstream>
 #include <limits>
-
+#include <unistd.h>
 
 template <typename T>
 T read_until(const std::string& msg) noexcept
-{
-        T read;
-        std::string buffer;
-        std::istringstream sstream;
-        // Use the class instringstream which is a subclass of istream
-        
-        do {
-            sstream.clear();
-            // At each cycle reset the failbit
-            // Note that not .ignore() has to be called:
-            //     every time we reset the sstream
-            std::cout << msg; 
-            std::cin >> buffer;
-            sstream.str(buffer);
-            // Set the sstream to the (string) buffer just read
-            sstream >> read;
-            // Try to input to read the value
-        } while (sstream.fail() || !sstream.eof());
-        // until two condition are satisfied:
-        // 1. no failure happen
-        // 2. the stream arrived at the end of the file
-        //    (otherwhise "34r" is accepted)
-        
-        return read;
-}
-
-
-template <typename T>
-T read_until_bis(const std::string& msg) noexcept
 {
         T read;
         std::string buf_tail;
@@ -52,7 +22,8 @@ T read_until_bis(const std::string& msg) noexcept
                 // Ignore the rest of the string
                 continue;
             }
-            std::getline(std::cin, buf_tail);
+            if(!std::getline(std::cin, buf_tail))
+                continue;
             if(buf_tail.erase(buf_tail.find_last_not_of(null_c) + 1)
                     .erase(0, buf_tail.find_first_not_of(null_c)) == "")
                 // If the trim of the tail buffer is empty the read is correct
@@ -65,17 +36,14 @@ T read_until_bis(const std::string& msg) noexcept
 
 int main()
 {
-    int i = read_until<int>("Insert and integer: ");
-    double d = read_until<double>("Insert a dobule: ");
+    const std::string msg_int = isatty(fileno(stdin)) ? "Insert an integer: " : "";
+    const std::string msg_dbl = isatty(fileno(stdin)) ? "Insert a dobule: " : "";
+    // If it is piping do not cout insert message
+    int i = read_until<int>(msg_int);
+    double d = read_until<double>(msg_dbl);
     
     std::cout << "i= " << i << std::endl;
-    std::cout << "d= " << d << std::endl;
-    
-    i = read_until_bis<int>("Insert and integer: ");
-    d = read_until_bis<double>("Insert a dobule: ");
-    
-    std::cout << "i= " << i << std::endl;
-    std::cout << "d= " << d << std::endl;  
+    std::cout << "d= " << d << std::endl; 
 
     return 0;
 }
