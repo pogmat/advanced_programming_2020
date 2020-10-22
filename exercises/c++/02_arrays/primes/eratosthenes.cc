46a57ec000dc134d484ea5b89ac33cbf3008d704
Matteo Poggi <matteo.poggi.fi@gmail.com>
2020-10-22 03:09:43 +0200

    Improve eratosthenes.cc

--- exercises/c++/02_arrays/primes/eratosthenes.cc
+++ exercises/c++/02_arrays/primes/eratosthenes.cc
@@ -8,21 +8,22 @@ int main()
     std::cout << "n_max= ";
     std::cin >> n_max;
 
-    bool* sieve{new bool[n_max - 1]};
-    for (uint64_t i{0}; i < n_max; ++i)
+    bool* sieve{new bool[n_max / 2 - 1]};
+    for (uint64_t i{0}; i < n_max / 2; ++i)
         sieve[i] = true;
 
     const u_int64_t n_sqrt{static_cast<uint64_t>(std::ceil(std::sqrt(n_max)))};
-    for (uint64_t i{2}; i < n_sqrt; ++i) {
-        if (sieve[i - 2]) {
-            for (uint64_t j{i * i}; j <= n_max; j += i)
-                sieve[j - 2] = false;
+    for (uint64_t i{3}; i < n_sqrt; i += 2) {
+        if (sieve[(i - 3) / 2]) {
+            for (uint64_t j{i * i}; j <= n_max; j += 2 * i)
+                sieve[(j - 3) / 2] = false;
         }
     }
 
-    for (uint64_t i{0}; i < n_max - 1; ++i)
+    std::cout << 2 << std::endl;
+    for (uint64_t i{0}; i < n_max / 2 - 1; ++i)
         if (sieve[i])
-            std::cout << i + 2 << std::endl;
+            std::cout << 2 * i + 3 << std::endl;
 
     return 0;
 }
